36bd1443cde01d8d6eca1cea963d109043efff54
meeta haldar <meetahaldar1001@gmail.com>
2021-11-22 23:37:01 +0530

    completed project

--- index.js
+++ index.js
@@ -54,27 +54,24 @@ generateBtn.addEventListener("click",() => {
     console.log(randomNumber);
 
 // for showing gif 
-if(randomNumber>50 && randomNumber<89 )
+if(randomNumber>50 && randomNumber<79 )
 {
-    document.querySelector("p").innerHTML=("You are falling in love = ") + randomNumber+"%";
+    document.querySelector("p").innerHTML="'"+ myInput1.value + " + "+ myInput2.value + "' "+(" ( You are falling in love ) = ") + randomNumber+"%";
     document.querySelector('img').src='https://thumbs.gfycat.com/EllipticalSnoopyCrocodile-size_restricted.gif';
 
 }
-else if(randomNumber>90)
+else if(randomNumber>80)
 {
-    document.querySelector("p").innerHTML=("True love is there = ") + randomNumber+"%";
+    document.querySelector("p").innerHTML= "'" +myInput1.value + " + "+ myInput2.value+"' " + (" ( True love is there ) = ") + randomNumber+"%";
     document.querySelector('img').src='https://c.tenor.com/0aJdWy--KOcAAAAC/turulove-true-love.gif';
 
 }
 else{
-    document.querySelector("p").innerHTML=("Fake love!! = ") + randomNumber+"%";
+    document.querySelector("p").innerHTML= "'"+ myInput1.value + " + "+ myInput2.value+"' " +(" ( Fake love!! ) = ") + randomNumber+"%";
     document.querySelector('img').src='https://i0.wp.com/www.eventstodayz.com/wp-content/uploads/2020/01/Heart-broken-gif.gif?fit=220%2C203&ssl=1';
 
 }
 
 });
 
-myInput1.addEventListener('keydown', () => {
-   console.log(myInput1);
-    
-  });
+
